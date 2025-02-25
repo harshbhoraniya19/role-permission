64b64420a4f8369db030ac1e883079a146577c87
harsh.bhoraniya <harsh.bhoraniya@tatvasoft.com>
2025-02-25 11:40:26 +0600

    Add Provider

---
+++ src/RolePermissionServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace harsh\LaravelRolePermission;
+
+use App\Http\Middleware\RoleMiddleware;
+use harsh\LaravelRolePermission\Middleware\PermissionMiddleware;
+use Illuminate\Support\ServiceProvider;
+
+class RolePermissionServiceProvider extends ServiceProvider
+{
+    private $_packageTag = 'laravelroles';
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @param \Illuminate\Routing\Router $router The router
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
+        $this->app['router']->aliasMiddleware('permission', PermissionMiddleware::class);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+}
